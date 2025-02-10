35591280774d0adb51756610fd21a5280589ceaf
Srinath Mannam <mvsrinath2009@gmail.com>
2025-02-10 11:42:03 +0100

    reduced code

--- src/ts/interfaces/process.ts
+++ src/ts/interfaces/process.ts
@@ -1,23 +1,7 @@
 import { GLOBALENUMS } from "../global-enums";
 
 export interface Process {
-  id?: number;
+  id: number;
   processName?: string;
-  children?: string[];
-  dateCreated?: Date;
-  lastChanged?: Date;
-  description?: string;
-  participant?: string;
-  owner?: string;
-  lastModifiedBy?: string;
-  processOwnerEmail?: string;
-  attachments?: string;
-  isInModeling?: boolean;
-  versionNumber?: number;
-  sendTo?: string;
-  resourceUri?: string;
-  linkText?: string;
-  linkUrl?: string;
   processMapType?: GLOBALENUMS.PROCESSMAPNODETYPE;
-  [index: string]: any;
 }
